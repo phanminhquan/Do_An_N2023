25b5f314c53d604706848ef4617f11215ea94e10
phanminhquan <2051052110quan@ou.edu.vn>
2024-03-08 11:55:50 +0700

    update captcha

--- Client/src/pages/LoginPage.js
+++ Client/src/pages/LoginPage.js
@@ -1,14 +1,24 @@
-import { useContext, useState,useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Form, Link, Navigate, useNavigate } from 'react-router-dom';
 
 // @mui
 import { styled } from '@mui/material/styles';
-import {  Container, Typography, Divider, Stack, Button, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Divider,
+  Stack,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Checkbox,
+} from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-import cookie from "react-cookies";
+import cookie from 'react-cookies';
 import { setGlobalState } from '..';
-import Apis, { authApi, endpoints } from '../configs/Apis'
+import Apis, { authApi, endpoints } from '../configs/Apis';
 import { MyUserContext } from '../App';
 
 // hooks
@@ -16,15 +26,6 @@ import useResponsive from '../hooks/useResponsive';
 // components
 import Logo from '../components/logo';
 import Iconify from '../components/iconify';
-
-
-
-
-
-
-
-
-
 
 // ----------------------------------------------------------------------
 
@@ -53,59 +54,83 @@ const StyledContent = styled('div')(({ theme }) => ({
   flexDirection: 'column',
   padding: theme.spacing(12, 0),
 }));
-
+const divStyleBoxCaptcha = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+};
+const captchaTextStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  border: '1px solid #D2D2D2',
+  width: '250px',
+  borderRadius: '5px',
+  fontFamily: 'cursive',
+  fontSize: '25px',
+  color: '#A0A0A0',
+  letterSpacing: '3px',
+  textDecoration: 'line-through',
+};
+const captchaButtonStyle = {
+  backgroundColor: '#2065D1',
+  color: 'white',
+  height: '30px',
+  width: '30px',
+};
 // ----------------------------------------------------------------------
 
 export default function LoginPage() {
+  const randomcaptcha = Math.random().toString(36).slice(8);
+  const refreshCap = () => {
+    setcaptcha(Math.random().toString(36).slice(8));
+  };
+  const [captcha, setcaptcha] = useState(randomcaptcha);
   const navigate = useNavigate();
-  document.body.classList.add("active-modal")
+  document.body.classList.add('active-modal');
 
   const [showPassword, setShowPassword] = useState(false);
 
-
   useEffect(() => {
-    cookie.save("user","");
-    cookie.save("token","")
-   },[])
+    cookie.save('user', '');
+    cookie.save('token', '');
+  }, []);
   const handleClick = () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const captchatext = document.getElementById('captcha').value;
     const process = async () => {
       try {
         const res = await Apis.post(endpoints.login, {
-          "email": email,
-          "password": password
-        }); 
-        cookie.save("token", res.data.jwtToken);
-        console.log(res.status)
-        if(res.status === 200){
-          setGlobalState("isAuthorized",true)
-        }
-      
-        const data = await authApi().get(endpoints.current_user);
-        const expirationOfToken = await Apis.get(`${endpoints.getExpirationDate}/${res.data.jwtToken}`)
-        cookie.save("expiration", expirationOfToken.data);
-        cookie.save("user", data);
-        setGlobalState("user",data);
-        dispatch({
-          "type": "login",
-          "payload": data.data
+          email,
+          password,
         });
-        
-        
+        cookie.save('token', res.data.jwtToken);
+        console.log(res.status);
+        if (res.status === 200 && captchatext === document.getElementById('textcap').textContent) {
+          setGlobalState('isAuthorized', true);
+          const data = await authApi().get(endpoints.current_user);
+          const expirationOfToken = await Apis.get(`${endpoints.getExpirationDate}/${res.data.jwtToken}`);
+          cookie.save('expiration', expirationOfToken.data);
+          cookie.save('user', data);
+          setGlobalState('user', data);
+          dispatch({
+            type: 'login',
+            payload: data.data,
+          });
+        } else {
+          alert('sai mã');
+        }
+      } catch (error) {
+        alert('Tài khoản hoặc mật khẩu không khả dụng');
       }
-
-      catch (error) { alert("Tài khoản hoặc mật khẩu không khả dụng") }
-    }
+    };
     process();
-    // navigate('/dashboard', { replace: true });
   };
-
 
   const mdUp = useResponsive('up', 'md');
   const [user, dispatch] = useContext(MyUserContext);
-  if (user != null)
-    return <Navigate to="/" />
+  if (user != null) return <Navigate to="/" />;
   return (
     <>
       <Helmet>
@@ -138,17 +163,16 @@ export default function LoginPage() {
 
             <Typography variant="body2" sx={{ mb: 5 }}>
               Don’t have an account? {''}
-              <Link variant="subtitle2" to = {"/register"} >Get started</Link>
+              <Link variant="subtitle2" to={'/register'}>
+                Get started
+              </Link>
             </Typography>
 
-          
-
-          
             <Stack spacing={3}>
-              <TextField id='email' name="email" label="Email address" />
+              <TextField id="email" name="email" label="Email address" />
 
               <TextField
-                id='password'
+                id="password"
                 name="password"
                 label="Password"
                 type={showPassword ? 'text' : 'password'}
@@ -162,8 +186,27 @@ export default function LoginPage() {
                   ),
                 }}
               />
+              <div className="box-captcha" style={divStyleBoxCaptcha}>
+                <div className="classcaptcha" id="textcap" style={captchaTextStyle}>
+                  {captcha}
+                  <Button onClick={refreshCap} style={captchaButtonStyle}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-clockwise"
+                      viewBox="0 0 16 16"
+                    >
+                      <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+                      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+                    </svg>
+                  </Button>
+                </div>
+                <TextField id="captcha" name="captcha" label="nhập mã xác thực" />
+              </div>
             </Stack>
-                <br/>
+            <br />
             <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
               Login
             </LoadingButton>
